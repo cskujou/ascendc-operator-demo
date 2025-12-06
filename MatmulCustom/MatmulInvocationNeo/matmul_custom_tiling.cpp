3aef2f85f283d75fa01202a3fa0f0baff77674b0
cskujou <1217117988@qq.com>
2025-12-06 15:50:52 +0800

    feat(add): implement MatmulInvocation (Kernel Invocation)

---
+++ MatmulCustom/MatmulInvocationNeo/matmul_custom_tiling.cpp
@@ -0,0 +1,74 @@
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+
+#include "tiling/platform/platform_ascendc.h"
+#include "tiling/tiling_api.h"
+using namespace matmul_tiling;
+using namespace std;
+
+/**
+ * @brief  Generate matmul tiling.
+ * @param  socVersion: Platform socversion.
+ * @param  tilingBuf data buffer.
+ */
+void GenerateTiling(const char* socVersion, uint8_t* tilingBuf) {
+    constexpr int32_t M = 2048;
+    constexpr int32_t N = 1024;
+    constexpr int32_t K = 512;
+
+    TPosition leftPosition = TPosition::GM;
+    CubeFormat leftFormat = CubeFormat::ND;
+    DataType leftDtype = DataType::DT_FLOAT16;
+    bool isTransA = false;
+
+    TPosition rightPosition = TPosition::GM;
+    CubeFormat rightFormat = CubeFormat::ND;
+    DataType rightDtype = DataType::DT_FLOAT16;
+    bool isTransB = false;
+
+    TPosition resultPosition = TPosition::GM;
+    CubeFormat resultFormat = CubeFormat::ND;
+    DataType resultDtype = DataType::DT_FLOAT;
+
+    bool isBias = false;
+
+    constexpr int32_t SINGLECORE_M = 512;
+    constexpr int32_t SINGLECORE_N = 512;
+
+    optiling::TCubeTiling tilingData;
+    auto ascendcPlatform = platform_ascendc::PlatformAscendCManager::GetInstance(socVersion);
+    MultiCoreMatmulTiling tilingApi(*ascendcPlatform);
+
+    printf("ascendcPlatform->GetCoreNumAiv() = %u\n", ascendcPlatform->GetCoreNumAiv());
+    tilingApi.SetDim(
+        ascendcPlatform->GetCoreNumAiv());
+    tilingApi.SetAType(leftPosition, leftFormat, leftDtype, isTransA);
+    tilingApi.SetBType(rightPosition, rightFormat, rightDtype, isTransB);
+    tilingApi.SetCType(resultPosition, resultFormat, resultDtype);
+
+    tilingApi.SetOrgShape(M, N, K);
+    tilingApi.SetShape(M, N, K);
+    if (ascendcPlatform->GetSocVersion() == platform_ascendc::SocVersion::ASCEND310P) {
+        tilingApi.SetSingleShape(SINGLECORE_M, SINGLECORE_N, -1);
+        int32_t mBlockNum = M / SINGLECORE_M;
+        int32_t nBlockNum = N / SINGLECORE_N;
+        tilingApi.SetDim(mBlockNum * nBlockNum);
+    }
+    tilingApi.SetBias(isBias);
+    tilingApi.SetBufferSpace(-1, -1, -1);
+
+    int64_t res = tilingApi.GetTiling(tilingData);
+    if (res == -1) {
+        std::cout << "gen tiling failed" << std::endl;
+    }
+    uint32_t tcubeTilingSize = tilingData.GetDataSize();
+    tilingData.SaveToBuffer(tilingBuf, tcubeTilingSize);
+
+    uint64_t localMemSize;
+    ascendcPlatform->GetCoreMemSize(platform_ascendc::CoreMemType::UB, localMemSize);
+    *reinterpret_cast<uint64_t*>(tilingBuf + tcubeTilingSize) = localMemSize;
+    return;
+}
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # 自用的昇腾 Ascend C 算子开发示例仓库
 
 用于记录开发一些常用算子过程中形成的工程，以方便后续定制算子复用已有的代码。
+
+
+## 环境搭建
+
+硬件：香橙派 AI pro (Ascend310B1)
+
+**TensorFlow**
+
+```
+pip install tensorflow-aarch64
+```
